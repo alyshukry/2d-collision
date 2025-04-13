26cc11bd3b87587289706075355e84dbfa3e54aa
Aly Shukry <alyshukry.dev@gmail.com>
2025-04-13 21:55:14 +0200

    oopsie

--- physics.js
+++ physics.js
@@ -37,8 +37,8 @@ class Particle {
 
         // Adds acceleration to the velocity
         if (gyroscope.frontToBack) {
-            this.velocityX += gyroscope.frontToBack / 90 * 0.35
-            this.velocityY += gyroscope.leftToRight / 90 * 0.35
+            this.velocityX += gyroscope.leftToRight / 90 * 0.35
+            this.velocityY += gyroscope.frontToBack / 90 * 0.35
 
         } else {
             this.velocityX += accelerationX
